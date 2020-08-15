3930e20b1002fedc8e6fde09ce34e0c301206e33
MaoLongLong <382084620@qq.com>
2020-08-15 12:41:24 +0800

    :construction: Improve position api

--- src/main/java/com/csl/vhr/controller/system/basic/PositionController.java
+++ src/main/java/com/csl/vhr/controller/system/basic/PositionController.java
@@ -1,14 +1,13 @@
 package com.csl.vhr.controller.system.basic;
 
 
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.csl.vhr.entity.Position;
 import com.csl.vhr.entity.RespBean;
 import com.csl.vhr.service.PositionService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -33,33 +32,29 @@ public class PositionController {
 
     @PostMapping("")
     public RespBean addPosition(@RequestBody Position position) {
+        position.setEnabled(true);
+        position.setCreateDate(LocalDateTime.now());
         boolean ok = positionService.save(position);
         if (ok) {
-            return RespBean.ok("添加成功",
-                    positionService.getById(position.getId()));
+            return RespBean.ok("添加成功", position);
         }
         return RespBean.error("添加失败");
     }
 
-    @PutMapping("/{id}")
-    public RespBean updatePosition(@PathVariable Integer id,
-                                   @RequestBody Position position) {
-
-        LambdaUpdateWrapper<Position> wrapper = Wrappers.lambdaUpdate();
-        wrapper.eq(Position::getId, id)
-                .set(Position::getName, position.getName())
-                .set(Position::getEnabled, position.getEnabled());
-
-        boolean ok = positionService.update(wrapper);
+    @PutMapping("")
+    public RespBean updatePosition(@RequestBody Position position) {
+        if (position.getEnabled() == null) {
+            position.setEnabled(true);
+        }
+        boolean ok = positionService.updateById(position);
         if (ok) {
-            return RespBean.ok("修改成功",
-                    positionService.getById(id));
+            return RespBean.ok("修改成功", position);
         }
         return RespBean.error("修改失败");
     }
 
     @DeleteMapping("/{id}")
-    public RespBean deletePosition(@PathVariable Integer id) {
+    public RespBean deletePosition(@PathVariable("id") Integer id) {
         boolean ok = positionService.removeById(id);
         if (ok) {
             return RespBean.ok("删除成功");
